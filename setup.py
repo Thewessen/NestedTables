7ebda210ec9b1dc83f2282bdc32ef61ef23006f3
Thewessen <sthewessen@gmail.com>
2019-05-19 11:55:42 +0200

    Working on upload to PyPi

--- setup.py
+++ setup.py
@@ -6,17 +6,16 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-# TODO: finish url
 setup(
-    name='Nestedtablescolumnprint',
-    version='1.0',
+    name='Nested tables columnprint',
+    version='1.0.0',
     author='SamuelThewessen',
     author_email='sthewessen@gmail.com',
-    description=('Amultipletablescreationtoolforprinting'
+    description=('A multiple tables creation tool for printing'
                  ' multidimensional lists into nice column output'),
     license='MIT',
     keywords='columnprint table nestedtabel',
-    url='http://github.com/Thewessen/',
+    url='http://github.com/Thewessen/NestedTables',
     packages='tables',
     long_discription=read('README.md'),
     classifiers=[
